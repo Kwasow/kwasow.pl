38fa40a5c51042b0bd1b655b7c10e1ccb51c45e9
Karol Wąsowski <wasowski02@protonmail.com>
2023-07-03 12:17:41 +0200

    Added reanimated tile

--- src/components/Icon.ts
+++ src/components/Icon.ts
@@ -1,9 +1,10 @@
 import { styled } from 'styled-components'
 
 export const Icon = styled.img`
-  width: 150x;
+  width: 150px;
   height: 150px;
   background-color: ${props => props.theme.colors.onBackground};
   padding: 20px;
   border-radius: 20px;
+  object-fit: contain;
 `
