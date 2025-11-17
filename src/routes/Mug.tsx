972977f73cc3fa0d0ce299b6b3c55cf0d4f213fd
Karol Wąsowski <wasowski02@protonmail.com>
2025-11-17 14:22:52 +0100

    Added mug subsite

---
+++ src/routes/Mug.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {
+  Content,
+  NavigationBar,
+} from '../components'
+
+export default function Mug() {
+  return (
+    <>
+      <NavigationBar title='Złodziej kubka'/>
+      <Content>
+        <p>Trwa namierzanie...</p>
+      </Content>
+    </>
+  )
+}
