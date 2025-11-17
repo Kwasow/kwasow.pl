ddf541e7ada9a4adb916855a1a1bebe3deee9f37
Karol Wąsowski <wasowski02@protonmail.com>
2025-11-17 14:29:06 +0100

    Updated dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,49 @@
+import { defineConfig } from "eslint/config";
+import react from "eslint-plugin-react";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([{
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+
+    plugins: {
+        react,
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+        },
+
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        indent: ["error", 2],
+        "linebreak-style": ["error", "unix"],
+        quotes: ["error", "single"],
+        semi: ["error", "never"],
+        "eol-last": ["error", "always"],
+        "max-len": ["error", {
+            code: 80,
+        }],
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+    },
+}]);
